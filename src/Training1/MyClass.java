1a690938a1ac23f0bc013b587b543e28a578cb54
pavan <pavan@Shwetha>
2022-07-18 18:10:57 -0400

    Testing Github commit cmd

--- src/Training1/MyClass.java
+++ src/Training1/MyClass.java
@@ -3,7 +3,7 @@ package Training1;
 public class MyClass extends Thread {
 	public static void main(String[] args) {
 		
-		
+		System.out.println("This is for testing github");
 		MyClass myObj = new MyClass();
 	    myObj.start();
 	    System.out.println("This code is outside of the thread");
