d00433f9b6a143ec60603a7b0859bc9040672b0b
pavan <pavan@Shwetha>
2022-07-18 18:12:50 -0400

    hurray

--- src/Training1/MyClass.java
+++ src/Training1/MyClass.java
@@ -4,6 +4,7 @@ public class MyClass extends Thread {
 	public static void main(String[] args) {
 		
 		System.out.println("This is for testing github");
+		System.out.println("Hurray we did it");
 		MyClass myObj = new MyClass();
 	    myObj.start();
 	    System.out.println("This code is outside of the thread");
