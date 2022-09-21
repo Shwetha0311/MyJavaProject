eea1da20bbb80e5ce4e2b07de9ded0fab868168e
Shwetha <shwetha.311nov@gmail.com>
2022-09-20 22:04:57 -0400

    Practice with Jenkins

---
+++ src/Practice/Area.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class Area {
+
+	int radius=7,side=8;
+	public void circle()
+	{
+		int area;
+		area = (22/7)*radius*radius;
+		System.out.println("The area of circle is :"+ area);
+	}
+	public void square()
+	{
+		int area;
+		area = side*side;
+		System.out.println("The area of circle is :"+ area);
+	}
+}
+
---
+++ src/Practice/MainApplication.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+
+		Area obj = new Area();
+		obj.circle();
+		obj.square();
+	}
+
+}
